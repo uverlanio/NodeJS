b6a2d1d033ee2b95b0f61eb9b55ff7d92b1ea3ef
usilva <usilva@a5solutions.com>
2019-09-10 20:10:03 -0300

    CRUD - API Restfull

--- apps/instagram_clone/api/server.js
+++ apps/instagram_clone/api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
 	mongodb = require('mongodb');
+	objectId = require('mongodb').ObjectId;
 
 var app = express();
 
@@ -28,17 +29,31 @@ app.get('/', function(req, res){
 app.get('/api', function(req, res){
 	db.open(function(err, mongoclient){
 		mongoclient.collection('postagens', function(err, collection){
-			collection.find({titulo: {$eq :'Oi'}}).toArray(function(err, records){
+			collection.find().toArray(function(err, records){
 				if(err){
 					res.json(err);
 				}else{
-					res.json(records[0]);
+					res.json(records);
 				}
 				mongoclient.close();
 			})
 		})
 	})
-})
+});
+
+app.get('/api/:id', function(req, res){
+	db.open(function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.find(objectId(req.params.id)).toArray(function(err, records){
+				if(err){
+					res.json(err);
+				}else{
+					res.json(records[0]);
+				}
+			})
+		})
+	})
+});
 
 app.post('/api', function(req, res){
 	var dados = req.body;
@@ -56,3 +71,39 @@ app.post('/api', function(req, res){
 		})
 	})
 });
+
+app.put('/api/:id', function(req, res){
+	db.open(function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.update(
+				{ _id : objectId(req.params.id)},
+				{ $set : {titulo : req.body.titulo}},
+				{},
+				function(err, records){
+				if(err){
+					res.json(err);
+				}else{
+					res.json(records);
+				}
+			
+			mongoclient.close();
+			})
+		})
+	})
+})
+
+app.delete('/api/:id', function(req, res){
+	db.open(function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.remove({ _id : objectId(req.params.id)},	function(err, records){
+				if(err){
+					res.json(err);
+				}else{
+					res.json(records);
+				}
+			
+			mongoclient.close();
+			})
+		})
+	})
+})
